c389016e029c279c81fe4c0e1d757949b5ef36c0
xristos <xristos@sdf.org>
2020-04-24 16:25:14 -0400

    Minor update

--- xterm-color.el
+++ xterm-color.el
@@ -32,7 +32,7 @@
 ;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;; POSSIBILITY OF SUCH DAMAGE.
-;;
+
 ;;; Commentary:
 ;;
 ;; Translate ANSI control sequences into text properties through state machine
