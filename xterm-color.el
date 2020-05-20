c6c305472830179a1cbd7162d7b05730917f639e
xristos <xristos@sdf.org>
2020-05-20 15:00:58 -0400

    Minor doc update

--- xterm-color.el
+++ xterm-color.el
@@ -38,7 +38,7 @@
 ;;
 ;; Translate ANSI control sequences into text properties through state machine
 ;; emulation. This provides a far more accurate, comprehensive result than
-;; `ansi-color.el' which is built-into Emacs, without compromising on performance.
+;; `ansi-color.el' that is built-into Emacs, without compromising on performance.
 ;;
 ;; Please see README.org for documentation including example configurations.
 ;;
